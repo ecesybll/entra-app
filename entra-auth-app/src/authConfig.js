dd5404b61ac9820c6e3654095b1935eea03a7c9a
ecesybll <166816065+ecesybll@users.noreply.github.com>
2025-11-16 15:06:14 +0300

    Update authConfig.js

--- entra-auth-app/src/authConfig.js
+++ entra-auth-app/src/authConfig.js
@@ -1,22 +1,15 @@
-const TENANT_NAME = "ecenursybllhwcustomertenant";
-const TENANT_DOMAIN = `${TENANT_NAME}.ciamlogin.com`;
-
 export const msalConfig = {
     auth: {
         clientId: "56f82b34-0e98-4568-b7d6-edcdd67c85c7",
         authority: `https://${TENANT_DOMAIN}`,
         knownAuthorities: [TENANT_DOMAIN],
-
-        // ❗ Kesin olarak sadece origin (CIAM bunu zorunlu kılıyor)
-        redirectUri: window.location.origin,
-        postLogoutRedirectUri: window.location.origin,
+        
+        // ✅ WEB için TAM PATH gerekiyor
+        redirectUri: `${window.location.origin}/auth-callback`,
+        postLogoutRedirectUri: `${window.location.origin}/`,
     },
     cache: {
-        cacheLocation: "sessionStorage",
-        storeAuthStateInCookie: false,
+        cacheLocation: "localStorage", // ✅ Web için localStorage
+        storeAuthStateInCookie: true,  // ✅ WEB için CRITICAL (true olmalı)
     }
-};
-
-export const loginRequest = {
-    scopes: ["openid", "profile", "email"]
 };
